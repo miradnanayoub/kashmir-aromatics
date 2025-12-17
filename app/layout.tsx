1210759b56aba301d9d68ad899d3dbdcf2b64d35
miradnanayoub <40299734+miradnanayoub@users.noreply.github.com>
2025-12-17 18:56:40 +0530

    added nextjs top-loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { CartProvider } from "@/context/CartContext"; // Connects the Cart Logic
 import CartDrawer from "@/components/CartDrawer";     // Connects the Sliding Sidebar
 import Footer from "@/components/Footer";
+import NextTopLoader from 'nextjs-toploader';
 
 // 1. Setup Montserrat (Body Text)
 const montserrat = Montserrat({
@@ -35,6 +36,17 @@ export default function RootLayout({
         suppressHydrationWarning={true}
         className={`${montserrat.variable} ${playfair.variable} antialiased bg-[#FAFAF9] text-[#1A1A1A]`}
       >
+        <NextTopLoader 
+          color="#d97706"  // This is your Amber-600 brand color
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={false} // Spinner is usually annoying, bar is better
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #d97706,0 0 5px #d97706"
+        />
         {/* We wrap the whole app in CartProvider so the cart works on every page */}
         <CartProvider>
           {children}
